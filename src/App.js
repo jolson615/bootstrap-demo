3118a50133762c009d38f25b841af15f70564350
Jeff Olson <jolson615@gmail.com>
2020-12-05 21:06:14 +0000

    adds react-bootstrap scripts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
-import './Bootstrap.css';
+// import './Bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from './Components/Card';
 import bunny from './Components/images/bunny.jpg';
+import CustomCarousel from './Components/CustomCarousel';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello CS + The City!</h1>
+      <CustomCarousel imgurl={bunny}/>
       <div className="container">
         <div className="row">
           <div className="col-sm">
